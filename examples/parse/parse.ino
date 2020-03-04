c4370c6ab3443e7e0aabfb002b917a6f3f4e0f31
mrWheel <5585427+mrWheel@users.noreply.github.com>
2020-03-04 09:56:13 +0100

    Add Belgisch telegram in parse.ino

--- examples/parse/parse.ino
+++ examples/parse/parse.ino
@@ -12,37 +12,83 @@
 #include "dsmr-be.h"
 
 // Data to parse
-const char raw[] =
-  "/KFM5KAIFA-METER\r\n"
+const char raw1[] =
+  "/FLU5\\253770234_A\r\n"
   "\r\n"
-  "1-3:0.2.8(40)\r\n"
-  "0-0:1.0.0(150117185916W)\r\n"
-  "0-0:96.1.1(0000000000000000000000000000000000)\r\n"
-  "1-0:1.8.1(000671.578*kWh)\r\n"
-  "1-0:1.8.2(000842.472*kWh)\r\n"
-  "1-0:2.8.1(000000.000*kWh)\r\n"
-  "1-0:2.8.2(000000.000*kWh)\r\n"
+  "0-0:96.1.4(50213)\r\n"
+  "0-0:96.1.1(3153414731313030303236393436)\r\n"
+  "0-0:1.0.0(190919165430S)\r\n"
+  "1-0:1.8.1(000031.250*kWh)\r\n"
+  "1-0:1.8.2(000040.792*kWh)\r\n"
+  "1-0:2.8.1(000061.957*kWh)\r\n"
+  "1-0:2.8.2(000023.548*kWh)\r\n"
   "0-0:96.14.0(0001)\r\n"
-  "1-0:1.7.0(00.333*kW)\r\n"
+  "1-0:1.7.0(00.342*kW)\r\n"
   "1-0:2.7.0(00.000*kW)\r\n"
-  "0-0:17.0.0(999.9*kW)\r\n"
+  "1-0:32.7.0(236.7*V)\r\n"
+  "1-0:31.7.0(002*A)\r\n"
   "0-0:96.3.10(1)\r\n"
-  "0-0:96.7.21(00008)\r\n"
-  "0-0:96.7.9(00007)\r\n"
-  "1-0:99.97.0(1)(0-0:96.7.19)(000101000001W)(2147483647*s)\r\n"
-  "1-0:32.32.0(00000)\r\n"
-  "1-0:32.36.0(00000)\r\n"
-  "0-0:96.13.1()\r\n"
+  "0-0:17.0.0(999.9*kW)\r\n"
+  "1-0:31.4.0(999*A)\r\n"
   "0-0:96.13.0()\r\n"
-  "1-0:31.7.0(001*A)\r\n"
-  "1-0:21.7.0(00.332*kW)\r\n"
-  "1-0:22.7.0(00.000*kW)\r\n"
   "0-1:24.1.0(003)\r\n"
-  "0-1:96.1.0(0000000000000000000000000000000000)\r\n"
-  "0-1:24.2.1(150117180000W)(00473.789*m3)\r\n"
+  "0-1:96.1.1(37464C4F32313139303035343533)\r\n"
   "0-1:24.4.0(1)\r\n"
-  "!6F4A\r\n";
+  "0-1:24.2.3(190919165007S)(00001.315*m3)\r\n"
+  "!BFE7\r\n";
 
+const char raw2[] =
+  "/FLU5\\253770234_A\r\n"
+  "\r\n"
+  "0-0:96.1.4(50213)\r\n"
+  "0-0:96.1.1(3153414731313030303331373636)\r\n"
+  "0-0:1.0.0(200104202329W)\r\n"
+  "1-0:1.8.1(000180.802*kWh)\r\n"
+  "1-0:1.8.2(000161.920*kWh)\r\n"
+  "1-0:2.8.1(000000.001*kWh)\r\n"
+  "1-0:2.8.2(000000.025*kWh)\r\n"
+  "0-0:96.14.0(0002)\r\n"
+  "1-0:1.7.0(00.364*kW)\r\n"
+  "1-0:2.7.0(00.000*kW)\r\n"
+  "1-0:32.7.0(240.9*V)\r\n"
+  "1-0:31.7.0(001*A)\r\n"
+  "0-0:96.3.10(1)\r\n"
+  "0-0:17.0.0(999.9*kW)\r\n"
+  "1-0:31.4.0(999*A)\r\n"
+  "0-0:96.13.0()\r\n"
+  "0-1:24.1.0(003)\r\n"
+  "0-1:96.1.1(37464C4F32313139303935333131)\r\n"
+  "0-1:24.4.0(1)\r\n"
+  "0-1:24.2.3(200104202004W)(00385.264*m3)\r\n"
+  "!A2CF\r\n";
+  
+const char raw3[] =
+  "/FLU5\\253770234_A\r\n"
+  "\r\n"
+  "0-0:96.1.4(50213)\r\n"
+  "0-0:96.1.1(3153414731313333333333333333)\r\n"
+  "0-0:1.0.0(191219105454W)\r\n"
+  "1-0:1.8.1(000000.915*kWh)\r\n"
+  "1-0:1.8.2(000001.955*kWh)\r\n"
+  "1-0:2.8.1(000000.000*kWh)\r\n"
+  "1-0:2.8.2(000000.030*kWh)\r\n"
+  "0-0:96.14.0(0001)\r\n"
+  "1-0:1.7.0(00.000*kW)\r\n"
+  "1-0:2.7.0(00.000*kW)\r\n"
+  "1-0:32.7.0(228.3*V)\r\n"
+  "1-0:31.7.0(000*A)\r\n"
+  "0-0:96.3.10(1)\r\n"
+  "0-0:17.0.0(999.9*kW)\r\n"
+  "1-0:31.4.0(999*A)\r\n"
+  "0-0:96.13.0(466C7373697373)\r\n"
+  "0-1:24.1.0(003)\r\n"
+  "0-1:96.1.1(37464C4F37373737373737373737)\r\n"
+  "0-1:24.4.0(1)\r\n"
+  "0-1:24.2.3(191219105038W)(00000.003*m3)\r\n"
+  "!0D91\r\n";
+
+
+  
 /**
  * Define the data we're interested in, as well as the datastructure to
  * hold the parsed data. This list shows all supported fields, remove
@@ -81,6 +127,9 @@ using MyData = ParsedData<
   /* FixedValue */ current_l1,
   /* FixedValue */ current_l2,
   /* FixedValue */ current_l3,
+  /* FixedValue */ fuse_treshold_l1,
+  /* FixedValue */ fuse_treshold_l2,
+  /* FixedValue */ fuse_treshold_l3,
   /* FixedValue */ power_delivered_l1,
   /* FixedValue */ power_delivered_l2,
   /* FixedValue */ power_delivered_l3,
@@ -139,17 +188,50 @@ struct Printer {
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("\r\nand then it begins ...\r\n\n");
 
   MyData data;
-  ParseResult<void> res = P1Parser::parse(&data, raw, lengthof(raw), true);
+  ParseResult<void> res;
+  
+  Serial.println("\r\n=============== raw1 Telegram ==============================\r\n");
+  Serial.print(raw1);
+  Serial.println("\r\n--------------- parsed -------------------------------------\r\n");
+  res = P1Parser::parse(&data, raw1, lengthof(raw1), true);
   if (res.err) {
     // Parsing error, show it
-    Serial.println(res.fullError(raw, raw + lengthof(raw)));
+    Serial.println(res.fullError(raw1, raw1 + lengthof(raw1)));
   } else {
     // Parsed succesfully, print all values
     data.applyEach(Printer());
   }
-}
+  
+  data = {};
+  Serial.println("\r\n=============== raw2 Telegram ==============================\r\n");
+  Serial.print(raw2);
+  Serial.println("\r\n--------------- parsed -------------------------------------\r\n");
+  res = P1Parser::parse(&data, raw2, lengthof(raw2), true);
+  if (res.err) {
+    // Parsing error, show it
+    Serial.println(res.fullError(raw2, raw2 + lengthof(raw2)));
+  } else {
+    // Parsed succesfully, print all values
+    data.applyEach(Printer());
+  }
+  
+  data = {};
+  Serial.println("\r\n=============== raw3 Telegram ==============================\r\n");
+  Serial.print(raw3);
+  Serial.println("\r\n--------------- parsed -------------------------------------\r\n");
+  res = P1Parser::parse(&data, raw3, lengthof(raw3), true);
+  if (res.err) {
+    // Parsing error, show it
+    Serial.println(res.fullError(raw3, raw3 + lengthof(raw3)));
+  } else {
+    // Parsed succesfully, print all values
+    data.applyEach(Printer());
+  }
+  
+} // setup()
 
 void loop () {
 }
